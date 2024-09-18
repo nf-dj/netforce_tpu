8dc9144015f0d0cb9e588d17739f016be10e6843
David Janssens <dj@x>
2024-09-18 17:35:10 +0700

    fp4 test

--- soc/nf_tpu/tests/synth_art7.sh
+++ soc/nf_tpu/tests/synth_art7.sh
@@ -2,8 +2,10 @@
 
 # Create a temporary Yosys script
 cat << EOF > temp_script.ys
-read_verilog ../fp8_e4m3.v
-synth_xilinx -family xc7 -top fp8_e4m3_pipelined_adder
+#read_verilog ../fp8_e4m3.v
+#synth_xilinx -family xc7 -top fp8_e4m3_pipelined_adder
+read_verilog ../fp4_e3m0.v
+synth_xilinx -family xc7 -top fp4_e3m0_adder
 stat
 EOF
 
--- soc/nf_tpu/tests/synth_ecp5.sh
+++ soc/nf_tpu/tests/synth_ecp5.sh
@@ -2,8 +2,10 @@
 
 # Create a temporary Yosys script
 cat << EOF > temp_script.ys
-read_verilog ../fp8_e4m3.v
-synth_ecp5 -top fp8_e4m3_pipelined_adder
+#read_verilog ../fp8_e4m3.v
+#synth_ecp5 -top fp8_e4m3_pipelined_adder
+read_verilog ../fp4_e3m0.v
+synth_ecp5 -top fp4_e3m0_adder
 stat
 EOF
 
