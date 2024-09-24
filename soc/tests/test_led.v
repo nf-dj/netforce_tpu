35169185f8ef67c9b4921c91dab1ae1d4f3a6e34
David Janssens <dj@x>
2024-09-25 02:14:53 +0700

    board

--- soc/tests/test_led.v
+++ soc/tests/test_led.v
@@ -1,12 +1,13 @@
 module led_control(
-    input wire clk,
     output reg led0,
-    output reg led1
+    output reg led1,
+    output reg led2
 );
 
     initial begin
-        led0 = 1'b0;
+        led0 = 1'b1;
         led1 = 1'b1;
+        led2 = 1'b1;
     end
 
 endmodule
